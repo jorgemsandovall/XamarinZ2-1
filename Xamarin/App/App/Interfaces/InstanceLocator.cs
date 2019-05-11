e93c7acf87580d6d445f4e42cfc9c4a794df9e4e
Jorge Sandoval <jorge_sandoval-98@hotmail.com>
2019-05-11 13:48:46 -0400

    con errores

---
+++ Xamarin/App/App/Interfaces/InstanceLocator.cs
@@ -0,0 +1,26 @@
+﻿namespace App.Interfaces
+{
+    using ViewModels;
+    public class InstanceLocator
+
+    {
+
+        #region Properties
+
+        public MainViewModel Main { get; set; }
+
+        #endregion       
+        #region Constructors
+
+        public InstanceLocator()
+
+        {
+
+            this.Main = new MainViewModel();
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Xamarin/App/App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,106 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace App.ViewModels
+{
+   public class LoginViewModel:BaseViewModel
+    {
+        #region Attributes
+
+        string email;
+
+        string password;
+
+        bool isrunning;
+
+        bool isenabled;
+
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                SetValue(ref this.email, value);
+            }
+        }
+        public string Password
+        {
+             
+                get
+            {
+                    return this.password;
+                }
+                set
+            {
+                    SetValue(ref this.password, value);
+            }
+            
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isrunning;
+            }
+            set
+            {
+                SetValue(ref this.isrunning , value);
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isenabled;
+            }
+            set
+            {
+                SetValue(ref this.isenabled, value);
+            }
+        }
+
+
+        #endregion
+
+
+        #region Commands
+        public ICommand LoginCommad
+        {
+            get
+            {
+                return new RelayCommand(cmdLogin);
+
+            }
+        }
+
+        private async void cmdLogin()
+        {
+            if (String.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Email empty", "Please input email", "Accept");
+
+                return;
+            }
+            if(String.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Passwprd empty", "Please input Password", "Accept");
+                return;
+
+            }
+
+            IsRunning = true;
+        }
+        #endregion
+
+    }
+}
